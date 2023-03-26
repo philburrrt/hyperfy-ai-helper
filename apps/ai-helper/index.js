6c4bed6074554068411875570ea864c7b4d6f022
philbert burrow <75766943+philburrrt@users.noreply.github.com>
2023-03-26 18:50:19 -0500

    send only to client who is tagging the agent

--- apps/ai-helper/index.js
+++ apps/ai-helper/index.js
@@ -5,13 +5,20 @@ const OPENAI_URL = 'https://api.openai.com/v1'
 const OPENAI_MODEL = 'gpt-3.5-turbo'
 
 // initial prompt needs to contain instructions on how to respond
-const INITIAL_PROMPT = (name, age, gender, personality, interests, prompt) => {
+const INITIAL_PROMPT = (
+  aiName,
+  age,
+  gender,
+  personality,
+  interests,
+  prompt
+) => {
   return [
     {
       role: 'user',
       content: `
         Respond as a fictional human with the following characteristics:
-        Name: ${name}
+        Name: ${aiName}
         Age: ${age}
         gender: ${gender}
         personality: ${personality}
@@ -25,7 +32,7 @@ const INITIAL_PROMPT = (name, age, gender, personality, interests, prompt) => {
 
 // sends a chat message that only the local user can see
 const debugChat = (msg, world) => {
-  world.chat(msg, false, true)
+  world.chat(msg, true)
 }
 
 export default function App() {
@@ -35,7 +42,7 @@ export default function App() {
   const [response, setResponse] = useState(null)
   // fields from in world editor
   const {
-    name,
+    aiName,
     age,
     gender,
     personality,
@@ -50,7 +57,7 @@ export default function App() {
   // when the response state changes, send response to chat and add it to the messages log
   useEffect(() => {
     if (!response) return
-    debugChat(`${name}: ${response.content}`, world)
+    debugChat(`${aiName}: ${response.content}`, world)
     setMessages([...messages, response])
   }, [response])
 
@@ -60,9 +67,13 @@ export default function App() {
     if (!world.isClient) return
     return world.on('chat', msg => {
       const { text, from } = msg
-      if (from === name) return
-      if (text.includes(`@${name}`)) {
-        const prompt = text.split(`@${name}`)[1]
+      const { name } = world.getAvatar()
+      if (from !== name) return
+      if (name === 'Anonymous')
+        return debugChat(`${aiName}: Please set your name, Anon`, world)
+      if (from === aiName) return
+      if (text.includes(`@${aiName}`)) {
+        const prompt = text.split(`@${aiName}`)[1]
         getResponse(prompt)
       }
     })
@@ -70,13 +81,13 @@ export default function App() {
 
   // send a request to the OpenAI API
   const getResponse = async prompt => {
-    if (!name || !age || !gender || !personality || !interests || !apiKey)
-      return debugChat(`${name}: Please fill out all fields`, world)
+    if (!aiName || !age || !gender || !personality || !interests || !apiKey)
+      return debugChat(`${aiName}: Please fill out all fields`, world)
     let message
     // if this is the first message, send the initial prompt
     if (!messages) {
       message = INITIAL_PROMPT(
-        name,
+        aiName,
         age,
         gender,
         personality,
@@ -94,7 +105,7 @@ export default function App() {
       message = [...messages, message]
     }
     try {
-      debugChat(`${name}: I'm thinking...`, world)
+      debugChat(`${aiName}: I'm thinking...`, world)
       const response = await world.http({
         method: 'POST',
         url: `${OPENAI_URL}/chat/completions`,
@@ -111,12 +122,12 @@ export default function App() {
         },
       })
       if (!response?.choices?.length)
-        return debugChat(`${name}: No response`, world)
+        return debugChat(`${aiName}: No response`, world)
       // ignoring all options except the first
       const answer = response?.choices[0].message
       setResponse(answer)
     } catch (e) {
-      console.error(`${name}:`, e.message, world)
+      console.error(`${aiName}:`, e.message, world)
     }
   }
 
@@ -134,7 +145,7 @@ export const getStore = (state = initialState) => {
     actions: {},
     fields: [
       {
-        key: 'name',
+        key: 'aiName',
         label: 'Name',
         type: 'text',
         placeholder: 'Required...',
